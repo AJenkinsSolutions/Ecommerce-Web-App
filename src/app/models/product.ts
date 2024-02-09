8a9b78b960a8a05109ec83d6e9ebbae1a29af66d
AJenkinsSolutions <ajenkinsuiw@gmail.com>
2024-02-09 11:01:32 -0600

    Add: Angular components

---
+++ src/app/models/product.ts
@@ -0,0 +1,15 @@
+export class Product {
+
+    private id: number; 
+    private name: string;
+    private image_url: string;
+
+
+    constructor(id: number, name: string, image_url: string){
+        this.id = id;
+        this.name = name;
+        this.image_url = image_url;
+
+        
+    }
+}
