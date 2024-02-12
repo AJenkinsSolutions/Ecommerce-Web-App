bbb705d25d7b256d0a725b4151a49c6afc66a247
AJenkinsSolutions <ajenkinsuiw@gmail.com>
2024-02-11 21:21:24 -0600

    Add: ProductService Initial Implementation

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent {
+
+
+  constructor(private productService: ProductService){}
+
+
+  getAllProducts(){
+
+    this.productService.getAllProducts()?.subscribe(array => {
+
+      console.log(array);
+
+    })
+
+
+
+  }
+
+
+
+
 
 }
--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
+import { environment } from 'src/environments/environment.development';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+
+  constructor(private http: HttpClient, ) { }
+
+
+
+
+
+
+  getAllProducts(): Observable<Product[]> | undefined{
+
+    console.log("INFO: Entered into getAllProducts in ProductService");
+    
+    const getAllEndPoint: string = '/products';
+
+    return this.http.get<Product[]>(environment.API_BASE_URL + getAllEndPoint);
+
+  }
+
+
 }
