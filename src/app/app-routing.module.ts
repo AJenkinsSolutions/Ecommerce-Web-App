4bccbd25ba64dce1cc50d5c70a918112dc957735
AJenkinsSolutions <ajenkinsuiw@gmail.com>
2024-02-15 11:17:21 -0600

    Add: cart route to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './product/product-list/product-list.component';
+import { CartComponent } from './cart/cart/cart.component';
 
 
 const routes: Routes = [
   // Creates redirect to our product route when no endpoint is given
   {path: '', redirectTo: '/products', pathMatch: 'full'},
-  {path: 'products', component: ProductListComponent}
-  //{path: 'cart', component: }
+  {path: 'products', component: ProductListComponent},
+  {path: 'cart', component: CartComponent}
 ];
 
 @NgModule({
